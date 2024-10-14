d102e2106af1451b56f42761921106501995acc0
Raphael James Cohn <raphael.cohn@stormmq.com>
2024-10-14 18:28:31 +0100

    Additional types

---
+++ src/types/sketchInternal/private/index.d.ts
@@ -0,0 +1,25 @@
+// This file is part of sketch-hct-plugin. It is subject to the license terms in the LICENSE file found in the top-level directory of this distribution and at https://raw.githubusercontent.com/raphaelcohn/sketch-hct-plugin/master/LICENSE. No part of sketch-hct-plugin, including this file, may be copied, modified, propagated, or distributed except according to the terms contained in the LICENSE file.
+// Copyright © 2024 The developers of sketch-hct-plugin. See the LICENSE file in the top-level directory of this distribution and at https://raw.githubusercontent.com/raphaelcohn/sketch-hct-plugin/master/LICENSE.
+
+/// <reference path="./MSModelObjectCommon.d.ts" />
+/// <reference path="./MSModelObject.d.ts" />
+
+/// <reference path="./_MSArtboardGroup.d.ts" />
+/// <reference path="./_MSBitmapLayer.d.ts" />
+/// <reference path="./_MSColor.d.ts" />
+/// <reference path="./_MSCurvePoint.d.ts" />
+/// <reference path="./_MSHotspotLayer.d.ts" />
+/// <reference path="./_MSLayer.d.ts" />
+/// <reference path="./_MSLayerGroup.d.ts" />
+/// <reference path="./_MSPage.d.ts" />
+/// <reference path="./_MSShapeGroup.d.ts" />
+/// <reference path="./_MSShapePathLayer.d.ts" />
+/// <reference path="./_MSSharedObject.d.ts" />
+/// <reference path="./MSSharedObject.d.ts" />
+/// <reference path="./_MSSharedStyle.d.ts" />
+/// <reference path="./_MSSliceLayer.d.ts" />
+/// <reference path="./_MSStyle.d.ts" />
+/// <reference path="./_MSStyledLayer.d.ts" />
+/// <reference path="./_MSSymbolInstance.d.ts" />
+/// <reference path="./_MSSymbolMaster.d.ts" />
+/// <reference path="./_MSTextLayer.d.ts" />
